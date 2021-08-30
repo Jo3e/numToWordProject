284b142f4443304a47f69804125e0bbf3f3412d2
Preshh0 <uniquep201@gmail.com>
2021-08-30 17:32:15 +0100

    Fixed it so the index numbers from 21 to 29 can work.

--- numToWord.py
+++ numToWord.py
@@ -1,6 +1,3 @@
-
-
- 
 digit = int(input("Enter a number to convert to words: "))
 
 def number_to_words(problem):
@@ -9,11 +6,11 @@ def number_to_words(problem):
         1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eigth", 9:"nine",  10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen",
          18:"eighteen",19:"nineteen",
          20:"twenty", 30:"thirty", 40:"fourty", 50:"fifty", 60:"sixty", 70:"seventy", 
-         80:"eigth", 90:"ninety", 100:"one hundred", 200:"two hundred"
+         80:"eigthy", 90:"ninety", 100:"one hundred", 200:"two hundred"
             }
     
 
-    for number,words in list(units.items())[1:19]:
+    for number,words in list(units.items())[1:20]:
         
 
          if digit == number:
@@ -21,11 +18,12 @@ def number_to_words(problem):
 
          else:
              pass
-    for number,words in list(units.items())[20:29]:
+    for number,words in list(units.items())[21:29]:
 
         if digit == number:
-            #print(units[number])
             print(units[number])
+        else:
+            pass
 
 number_to_words(digit)
 
