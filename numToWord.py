ae62632108b23a30e1367f54065befbc958eb000
Preshh0 <uniquep201@gmail.com>
2021-09-01 02:15:18 +0100

    There's this nasty error after line 35, but there's progress. It's to figure out how to get the program to take each value of the list that's converted to int to read through the dictionary and take the assigned value.

--- numToWord.py
+++ numToWord.py
@@ -1,12 +1,12 @@
 digit = int(input("Enter a number to convert to words: "))
 
 def number_to_words(problem):
-
+    global units
     units = {
-        1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eigth", 9:"nine",  10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen",
+        1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine",  10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen",
          18:"eighteen",19:"nineteen",
          20:"twenty", 30:"thirty", 40:"fourty", 50:"fifty", 60:"sixty", 70:"seventy", 
-         80:"eigthy", 90:"ninety", 100:"one hundred", 200:"two hundred"
+         80:"eighty", 90:"ninety", 100:"one hundred", 200:"two hundred"
             }
     
 
@@ -26,5 +26,38 @@ def number_to_words(problem):
             pass
 
 number_to_words(digit)
+x = str(digit)
+print(x)
+output = []
+for i in x:
+    output.append(i)
+print(output)
+
+if len(output) == 2:
+    for number, words in units.items():
+        output[0] = int()
+        if output[0] and output[1] == number:
+            print(units[words])
+#         print(units[words])
+#         text1 = output[0]
+#         text2 = output[1]
+#         print(f"Your number is {text1} and {text2}")
+# else:
+#     pass
+
+
+if len(output) == 3:
+    output[0] = list(units.items())[28:29]
+    tex1 = output[0]
+    output[1] = list(units.items())[21:27]
+    tex2 = output[2]
+    output[2] = list(units.items())[0:9]
+    tex3 = output[2]
+    print(f"Your number is {tex1}, {tex2} and {tex3}")
+
+else:
+    pass
+
+
 
 
