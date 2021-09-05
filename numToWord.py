2d00475530e066274bb835bc6e8f402aacb6879e
Preshh0 <uniquep201@gmail.com>
2021-09-05 03:24:16 +0100

    This works to an extent. The issue now is to figure out how to output 103 to one hundred and three. The commented out code in lines 63 and 64 is a trial, but it doesn't work. I'd keep trying though.

--- numToWord.py
+++ numToWord.py
@@ -1,13 +1,13 @@
 digit = input("Enter a number to convert to words: ")
 
 units = {
-        1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eigth", 9:"nine",  10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen",
+        1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine",  10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen", 17:"seventeen",
          18:"eighteen",19:"nineteen"}
          
 tens =  {
 
          20:"twenty", 30:"thirty", 40:"fourty", 50:"fifty", 60:"sixty", 70:"seventy", 
-         80:"eigth", 90:"ninety"}
+         80:"eight", 90:"ninety"}
          
 hundred = { 100:"one hundred", 200:"two hundred"
             }
@@ -19,12 +19,9 @@ def number_to_words(problem):
          print(units[int(digit)])
             
     elif len(digit) == 2:
-             
-
-    
-   
-
+            
         split_number = []
+
         for letters in digit:
             split_number.append(letters)
 
@@ -40,51 +37,39 @@ def number_to_words(problem):
                 split_tens = tens[first_letter]
 
         for num in units.keys():
-             second_letter = int(second_letter)
-             if second_letter == num:
+            second_letter = int(second_letter)
+            if second_letter == num:
                 global split_unit
                 split_unit = units[second_letter]
 
         print(split_tens,'-', split_unit)
 
         
-        # if len(split_number) == 3 and split_number[1] == '0':
+    if len(digit) == 3:
+        split_number = []
 
-            
-        #     first_letter = split_number[0] 
-            
-        #     second_letter = split_number[1] + '0'
-        #     merged_letters = first_letter + second_letter
-            
-        #     third_letter = split_number[2]
-            
+        for letters in digit:
+            split_number.append(letters)
+        
+        if len(split_number) == 3:
+            first_letter = split_number[0] + "00" 
+            second_letter = split_number[1] + "0"
+            third_letter = split_number[2]
+            # print(first_letter, second_letter, third_letter)
 
-        #     for num in hundred.keys():
-        #         merged_letters = int(merged_letters)
-        #         if merged_letters == num:
-        #             global split_hundred
-        #             split_hundred = hundred[merged_letters]
-
-        #     for num in units.keys():
-        #         third_letter = int(third_letter)
-        #         if third_letter == num:
-        #             global split_units
-        #             split_units = units[third_letter]
-
-        #     print(split_hundred, 'and',split_units)
-
-        # elif len(split_number) == 3 and split_number[1] != 0:
-        #     ''
-
-
-
-#         print(units[words])
-#         text1 = output[0]
-#         text2 = output[1]
-#         print(f"Your number is {text1} and {text2}")
-# else:
-#     pass
-
+        for num in split_number:
+            first_letter = int(first_letter)
+            second_letter = int(second_letter)
+            # if split_number[1] == int(0):
+            #     second_letter = "and"
+            third_letter = int(third_letter)
+            if first_letter == hundred.keys():
+                pass
+            if second_letter == tens.keys():
+                pass
+            if third_letter == units.keys():
+                pass
+        print(hundred[first_letter], "and", tens[second_letter], units[third_letter])
 
 number_to_words(digit)
 
